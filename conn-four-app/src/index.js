c6457e48b9b788e26a82ebba96363479efb59a90
Matthew Rubino <budlurr@gmail.com>
2021-01-09 16:54:09 -0500

    improved readability of front end code and added styling for tiles

--- conn-four-app/src/index.js
+++ conn-four-app/src/index.js
@@ -9,18 +9,66 @@ class Game extends React.Component
         super(props);
         this.state = {
             rows: props.rows,
-            columns: props.columns
+            columns: props.columns,
+            board: props.board,
+            active: true,
+            maximizingPlayer: true,
+            maximizingIsRed: true
         };
+    }
+
+    getColumn(k)
+    {
+        let column = [];
+
+        for (let i = 0; i < this.state.board.length; i++)
+        {
+            column.push(this.state.board[i][k]);
+        }
+
+        return column;
     }
 
     renderColumn(i)
     {
-        return <ul key={i} className="column"><button onClick={() => this.debugClick(i)}><Column index={i} total_height={this.props.rows} /></button></ul>;
+        let column = <Column index={i} total_height={this.props.rows} column={this.getColumn(i)} maxIsRed={this.state.maximizingIsRed}/>;
+        return <ul key={i} className="column"><button onClick={() => this.debugClick(i)}>{column}</button></ul>;
+    }
+
+    getMoveHeight(k)
+    {
+        let i = this.state.board.length - 1;
+
+        while (i >= 0)
+        {
+            if (!this.state.board[i][k])
+                return i;
+
+            i -= 1;
+        }
+
+        return i;
     }
 
     debugClick(i)
     {
-        console.log("You clicked on column " + i);
+        if (!this.state.active)
+            return;
+
+        let move = this.getMoveHeight(i);
+        if (move === -1)
+            return;
+
+        let chip = 0;
+        if (this.state.maximizingPlayer)
+            chip = 1;
+        else
+            chip = -1;
+        
+        let newBoard = this.state.board.slice();
+        newBoard[move][i] = chip;
+
+        this.setState({board: newBoard, maximizingPlayer: !this.state.maximizingPlayer})
     }
 
     render()
@@ -31,7 +79,7 @@ class Game extends React.Component
             elements.push(this.renderColumn(i));
         }
 
-        return elements;
+        return <div className="game">{elements}</div>;
     }
 }
 
@@ -49,7 +97,33 @@ class Column extends React.Component
 
     renderTile(i)
     {
-        return <li key={i} className="tile"><Tile height={i} /></li>
+        let tile = this.props.column[i];
+        let className = "tile";
+
+        if (tile === 1)
+        {
+            if (this.props.maxIsRed)
+            {
+                className += "-red";
+            }
+            else
+            {
+                className += "-yellow";
+            }
+        }
+        else if (tile === -1)
+        {
+            if (this.props.maxIsRed)
+            {
+                className += "-yellow";
+            }
+            else
+            {
+                className += "-red";
+            }
+        }
+
+        return <Tile key={i} class={className} />;
     }
 
     render()
@@ -64,20 +138,30 @@ class Column extends React.Component
     }
 }
 
-class Tile extends React.Component
+function Tile(props)
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            height: props.height
-        }
-    }
+    let className = props.class;
+    if (props.highlighted)
+        className += "-highlighted";
 
-    render()
-    {
-        return this.state.height;
-    }
+    return <li key={props.i} className={className}></li>
 }
 
-ReactDom.render(<div className="game"><Game rows={6} columns={7} /></div>, document.getElementById("root"));
+function generateBoard(rows, columns)
+{
+    let board = [];
+
+    for (let i = 0; i < rows; i++)
+    {
+        let row = [];
+        for (let k = 0; k < columns; k++)
+        {
+            row.push(0);
+        }
+        board.push(row);
+    }
+
+    return board;
+}
+
+ReactDom.render(<Game rows={6} columns={7} board={generateBoard(6,7)} />, document.getElementById("root"));
