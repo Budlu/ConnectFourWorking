edf09ea1e4eed9348ed132f2a211dbc4ba872d54
Matthew Rubino <budlurr@gmail.com>
2021-01-19 16:29:50 -0500

    improved evaluation function, refactored code, and cleared hashes

--- conn-four-venv/main.py
+++ conn-four-venv/main.py
@@ -25,42 +25,6 @@ def take_action(this_board, this_drop_height, maximizing_player, action):
     this_board[this_drop_height[action]][action] = maximizing_player - (not maximizing_player)
     this_drop_height[action] -= 1
     return this_board, this_drop_height
-
-def get_best_move(board, drop_height, maximizing_player):
-    valid_moves = get_valid_moves(drop_height)
-    greatest_win_depth = -1
-    win_index = -1
-    greatest_val = -math.inf
-    smallest_val = math.inf
-    best_move = 0
-
-    for action in valid_moves:
-        new_board, new_drop_height = take_action(copy.deepcopy(board), copy.deepcopy(drop_height), maximizing_player, action)
-        val, depth = minimax(new_board, new_drop_height, SEARCH_DEPTH - 1, -math.inf, math.inf, not maximizing_player, None, None)
-
-        if maximizing_player and val == WIN_VAL:
-            if depth > greatest_win_depth:
-                greatest_win_depth = depth
-                win_index = action
-        elif (not maximizing_player) and val == -WIN_VAL:
-            if depth > greatest_win_depth:
-                greatest_win_depth = depth
-                win_index = action
-        elif maximizing_player:
-            altered = val - (abs(3 - action) + (len(board) - drop_height[action]) // 2)
-            if altered > greatest_val:
-                greatest_val = altered
-                best_move = action
-        else:
-            altered = val + (abs(3 - action) + (len(board) - drop_height[action]) // 2)
-            if altered < smallest_val:
-                smallest_val = altered
-                best_move = action
-                
-    if win_index != -1:
-        return win_index
-    else:
-        return best_move
     
 def minimax(board, drop_height, depth, alpha, beta, maximizing_player, last_board, last_action):
     if last_board != None:
@@ -74,168 +38,161 @@ def minimax(board, drop_height, depth, alpha, beta, maximizing_player, last_boar
         val = evaluate(board)
         HASHES[board_int] = val
     
-    if depth == 0 or val == WIN_VAL or val == -WIN_VAL:
-        return val, depth
+    if depth == 0 or abs(val) == WIN_VAL:
+        return val, depth, 0
     
     if maximizing_player:
         max_eval = -math.inf
-        selected_depth = 0
+        selected_depth = -1
+        selected_action = 0
 
         for action in get_valid_moves(drop_height):
             board_copy = copy.deepcopy(board)
             drop_height_copy = copy.deepcopy(drop_height)
 
             new_board, new_drop_height = take_action(board_copy, drop_height_copy, maximizing_player, action)
-            this_val, eval_depth = minimax(new_board, new_drop_height, depth - 1, alpha, beta, False, board_int, action)
-            
-            if this_val > max_eval:
+            this_val, eval_depth, last_best_action = minimax(new_board, new_drop_height, depth - 1, alpha, beta, False, board_int, action)
+
+            if this_val == WIN_VAL:
+                if this_val > max_eval or eval_depth > selected_depth:
+                    selected_depth = eval_depth
+                    max_eval = this_val
+                    selected_action = action
+            elif this_val > max_eval:
                 max_eval = this_val
                 selected_depth = eval_depth
+                selected_action = action
 
             alpha = max([alpha, max_eval])
             if beta <= alpha:
                 break
 
-        return max_eval, selected_depth
+        return max_eval, selected_depth, selected_action
     else:
         min_eval = math.inf
-        selected_depth = 0
+        selected_depth = -1
+        selected_action = 0
 
         for action in get_valid_moves(drop_height):
             board_copy = copy.deepcopy(board)
             drop_height_copy = copy.deepcopy(drop_height)
 
             new_board, new_drop_height = take_action(board_copy, drop_height_copy, maximizing_player, action)
-            this_val, eval_depth = minimax(new_board, new_drop_height, depth - 1, alpha, beta, True, board_int, action)
+            this_val, eval_depth, last_best_action = minimax(new_board, new_drop_height, depth - 1, alpha, beta, True, board_int, action)
 
-            if this_val < min_eval:
-                min_eval = this_val
+            if depth == SEARCH_DEPTH:
+                print(action, this_val)
+
+            if this_val == -WIN_VAL:
+                if this_val < min_eval or eval_depth > selected_depth:
+                    selected_depth = eval_depth
+                    min_eval = this_val
+                    selected_action = action
+            elif this_val < min_eval:
                 selected_depth = eval_depth
+                min_eval = this_val
+                selected_action = action
 
             beta = min([beta, min_eval])
             if beta <= alpha:
                 break
             
-        return min_eval, selected_depth
+        return min_eval, selected_depth, selected_action
 
 def evaluate(board):
-    three_score = 0
+    evaluation = 0
     threats = np.zeros((6,7))
-    
-    # Horizontal win check
-    for val in [-1, 1]:
-        for i in range(len(board[0]) - 3):
-            for k in range(len(board)):
 
-                if(board[k][i] == val and board[k][i] == board[k][i + 1] and board[k][i + 1] == board[k][i + 2] and board[k][i + 2] == board[k][i + 3]):
-                    return WIN_VAL * val
+    # Horizontal checks
+    for i in range(len(board)):
+        for k in range(len(board[0]) - 3):
+            s = np.sum(board[i,k:k+4])
+            absS = abs(s)
+            sign = np.sign(s)
 
-                if((board[k][i] == val and board[k][i] == board[k][i + 1] and board[k][i + 1] == board[k][i + 2] and board[k][i + 3] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k][i + 1] and board[k][i + 1] == board[k][i + 3] and board[k][i + 2] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k][i + 2] and board[k][i + 2] == board[k][i + 3] and board[k][i + 1] == 0) or 
-                    (board[k][i + 1] == val and board[k][i + 1] == board[k][i + 2] and board[k][i + 2] == board[k][i + 3] and board[k][i] == 0)):
-                    three_score += val * (k // 2)
+            if absS == 4:
+                return WIN_VAL * sign
+            elif absS == 3:
+                evaluation += sign * (i // 2)
 
-                    if k < 5:
-                        for j in range(4):
-                            if board[k + 1][i + j] == 0:
-                                threats[k][i + j] = val
+                if i < 5:
+                    for j in range(4):
+                        if board[i + 1][k + j] == 0:
+                            threats[i][k + j] = sign
 
-    # Vertical win check
-    for val in [-1, 1]:            
-        for i in range(len(board[0])):
-            for k in range(len(board) - 3):
+    # Vertical checks     
+    for i in range(len(board) - 3):
+        for k in range(len(board[0])):
+            s = np.sum(board[i:i+4,k])
+            absS = abs(s)
+            sign = np.sign(s)
 
-                if(board[k][i] == val and board[k][i] == board[k + 1][i] and board[k + 1][i] == board[k + 2][i] and board[k + 2][i] == board[k + 3][i]):
-                    return WIN_VAL * val
-
-                if((board[k][i] == val and board[k][i] == board[k + 1][i] and board[k + 1][i] == board[k + 2][i] and board[k + 3][i] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 1][i] and board[k + 1][i] == board[k + 3][i] and board[k + 2][i] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 2][i] and board[k + 2][i] == board[k + 3][i] and board[k + 1][i] == 0) or 
-                    (board[k + 1][i] == val and board[k + 1][i] == board[k + 2][i] and board[k + 2][i] == board[k + 3][i] and board[k][i] == 0)):
-                    three_score += val
+            if absS == 4:
+                return WIN_VAL * sign
+            elif absS == 3:
+                evaluation += sign
                     
-    # TL to BR win check
-    for val in [-1, 1]:
-        for i in range(len(board[0]) - 3):
-            for k in range(len(board) - 3):
+    # TL to BR checks
+    for i in range(len(board) - 3):
+        for k in range(len(board[0]) - 3):
+            s = np.sum([board[i][k], board[i + 1][k + 1], board[i + 2][k + 2], board[i + 3][k + 3]])
+            absS = abs(s)
+            sign = np.sign(s)
 
-                if(board[k][i] == val and board[k][i] == board[k + 1][i + 1] and board[k + 1][i + 1] == board[k + 2][i + 2] and board[k + 2][i + 2] == board[k + 3][i + 3]):
-                    return WIN_VAL * val
+            if absS == 4:
+                return WIN_VAL * sign
+            elif absS == 3:
+                evaluation += sign
 
-                if((board[k][i] == val and board[k][i] == board[k + 1][i + 1] and board[k + 1][i + 1] == board[k + 2][i + 2] and board[k + 3][i + 3] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 1][i + 1] and board[k + 1][i + 1] == board[k + 3][i + 3] and board[k + 2][i + 2] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 2][i + 2] and board[k + 2][i + 2] == board[k + 3][i + 3] and board[k + 1][i + 1] == 0) or 
-                    (board[k + 1][i + 1] == val and board[k + 1][i + 1] == board[k + 2][i + 2] and board[k + 2][i + 2] == board[k + 3][i + 3] and board[k][i] == 0)):
-                    three_score += val
-                    if k < 2:
-                        for j in range(4):
-                            if board[k + 1 + j][i + j] == 0:
-                                threats[k + j][i + j] == val
-                                
-                    if board[k][i] == 0:
-                        threats[k][i] = val
-                    elif k > 0 and i > 0:
-                        if board[k - 1][i - 1] == 0 and board[k][i - 1] == 0:
-                            threats[k - 1][i - 1] = val
+                r = 4 if i < 2 else 3
+
+                for j in range(r):
+                    if board[i + j + 1][k + j] == 0:
+                        threats[i + j][k + j] = sign
                     
-    # TR to BL win check           
-    for val in [-1, 1]:
-        for i in range(len(board[0]))[3::]:
-            for k in range(len(board) - 3):
+    # TR to BL checks
+    for i in range(len(board) - 3):
+        for k in range(3, len(board[0])):
+            s = np.sum([board[i][k], board[i + 1][k - 1], board[i + 2][k - 2], board[i + 3][k - 3]])
+            absS = abs(s)
+            sign = np.sign(s)
 
-                if(board[k][i] == val and board[k][i] == board[k + 1][i - 1] and board[k + 1][i - 1] == board[k + 2][i - 2] and board[k + 2][i - 2] == board[k + 3][i - 3]):
-                    return WIN_VAL * val
+            if absS == 4:
+                return WIN_VAL * sign
+            elif absS == 3:
+                evaluation += sign
 
-                if((board[k][i] == val and board[k][i] == board[k + 1][i - 1] and board[k + 1][i - 1] == board[k + 2][i - 2] and board[k + 3][i - 3] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 1][i - 1] and board[k + 1][i - 1] == board[k + 3][i - 3] and board[k + 2][i - 2] == 0) or 
-                    (board[k][i] == val and board[k][i] == board[k + 2][i - 2] and board[k + 2][i - 2] == board[k + 3][i - 3] and board[k + 1][i - 1] == 0) or 
-                    (board[k + 1][i - 1] == val and board[k + 1][i - 1] == board[k + 2][i - 2] and board[k + 2][i - 2] == board[k + 3][i - 3] and board[k][i] == 0)):
-                    three_score += val
+                r = 4 if i < 2 else 3
 
-                    if k < 2:
-                        for j in range(4):
-                            if board[k + 1 + j][i - j] == 0:
-                                threats[k + j][i - j] == val
+                for j in range(r):
+                    if board[i + j + 1][k - j] == 0:
+                        threats[i + j][k - j] = sign
 
-                    if board[k][i] == 0:
-                        threats[k][i] = val
-                    elif k > 0 and i < 6:
-                        if board[k - 1][i + 1] == 0 and board[k][i + 1] == 0:
-                            threats[k - 1][i + 1] = val
-    
-    threat_score = 0
-    for i in range(len(board[0])):
-        this_col = 0
+    # Threat evaluation
+    for k in range(len(board[0])):
+        this_col = -1
         col_ind = 0
-        multiplier = 1
-        for k in range(len(board)):
-            if threats[k][i] != 0 and this_col == 0:
-                this_col = threats[k][i]
-                col_ind = k
-                
-            if threats[k][i] == -1 and this_col == -1:
-                if ((k - col_ind) % 2) == 1:
+        multiplier = 0
+
+        for i in range(len(board)):
+            if threats[i][k] == 0:
+                continue
+            elif this_col == -1:
+                this_col = threats[i][k]
+                col_ind = i
+                multiplier = SINGLE_THREAT_VAL
+            elif threats[i][k] == this_col:
+                if ((i - col_ind) % 2) == 1:
                     multiplier = MULTI_THREAT_VAL
                 else:
                     multiplier = SINGLE_THREAT_VAL
-                
-            if threats[k][i] == 1 and this_col == 1:
-                if ((k - col_ind) % 2) == 1:
-                    multiplier = MULTI_THREAT_VAL
-                else:
-                    multiplier = SINGLE_THREAT_VAL
-                
-            if threats[k][i] == 1 and this_col == -1:
-                this_col = 1
+            else:
+                this_col = threats[i][k]
                 multiplier = SINGLE_THREAT_VAL
-                
-            if threats[k][i] == -1 and this_col == 1:
-                this_col = -1
-                multiplier = SINGLE_THREAT_VAL
-        threat_score += this_col * multiplier   
+
+        evaluation += this_col * multiplier   
             
-    return three_score + threat_score
+    return evaluation
 
 def check_tie(drop_height):
     tie = True
@@ -244,15 +201,6 @@ def check_tie(drop_height):
             tie = False
             break
     return tie
-
-def player_one_win_percentage(board, drop_height, depth, maximizing_player):
-    new_board = copy.deepcopy(board)
-    new_drop_height = copy.deepcopy(drop_height)
-    
-    val, depth = minimax(new_board, new_drop_height, depth, -math.inf, math.inf, maximizing_player, None, None)
-    
-    p_one_percent = clamp(50 + (val * 2), 0, 100)
-    return p_one_percent
 
 def calculate_drop_height(board):
     ret_val = []
@@ -354,13 +302,14 @@ CORS(app)
 def get_percent():
     data = flask.request.get_json()
 
-    board = data
+    board = np.array(data)
     drop_height = calculate_drop_height(board)
     player = get_turn(board)
-    percent = player_one_win_percentage(board, drop_height, SEARCH_DEPTH, player)
-    move = get_best_move(board, drop_height, player)
 
-    return json.dumps({"percent": percent, "best": move})
+    val, eval_depth, move = minimax(board, drop_height, SEARCH_DEPTH, -math.inf, math.inf, player, None, None)
+    percent = clamp(50 + (val * 2), 0, 100)
+
+    return json.dumps({"percent": int(percent), "best": move})
 
 if __name__ == '__main__':
     atexit.register(write_hashes)
